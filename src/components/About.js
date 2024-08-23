8dd757556ac65c23cdc7ab520570189f9f1daf84
Israel Uzcategui <israel.uzcategui@outlook.com>
2024-08-23 12:07:46 -0400

    modified about

--- src/components/About.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const About = () => {
-    return (_jsxs("section", { id: "about", className: "my-5", children: [_jsx("h2", { className: "text-warning", children: "About Me" }), _jsx("p", { children: "Your brief bio goes here." })] }));
-};
-export default About;
