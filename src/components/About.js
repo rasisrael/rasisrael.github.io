f7e27847ea44553a677659e8115d83107d6aed13
Israel Uzcategui <israel.uzcategui@outlook.com>
2024-08-25 22:39:12 -0400

    transfer IDE

---
+++ src/components/About.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const About = () => {
+    return (_jsxs("section", { id: "about", className: "my-5", children: [_jsx("h2", { className: "text-warning", children: "About Me" }), _jsx("p", { children: "Hard working and passionate about technology, focused on steering my career into IT and Web Development. Thrive working in a team meeting diverse people." })] }));
+};
+export default About;
---
+++ src/components/Projects.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const Projects = () => {
+    return (_jsxs("section", { id: "projects", className: "my-5", children: [_jsx("h2", { className: "text-warning", children: "Projects" }), _jsx("div", { className: "card mb-3", children: _jsxs("div", { className: "card-body", children: [_jsx("h5", { className: "card-title", children: "Hotel Booking Website" }), _jsx("p", { className: "card-text", children: "Built with PHP, Laravel & MySQL." })] }) }), _jsx("div", { className: "card mb-3", children: _jsxs("div", { className: "card-body", children: [_jsx("h5", { className: "card-title", children: "Medical Booking System" }), _jsx("p", { className: "card-text", children: "Developed using React, Node.JS & MySQL." }), _jsx("a", { href: "https://github.com/rasisrael/MedAppSys", children: "View on GitHub" })] }) })] }));
+};
+export default Projects;
